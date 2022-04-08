b7ebb9b228574d40f4830c7961e7d14d773af3af
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2022-04-08 15:40:01 +0200

    Fix new pytorch torchvision efficientnet model

--- src/mllooper/models/efficientnet/model.py
+++ src/mllooper/models/efficientnet/model.py
@@ -1,23 +1,34 @@
 from functools import partial
-from typing import List, Any, Literal
+from typing import Any, Literal
 
 from torch import nn
 from torch.hub import load_state_dict_from_url
 from torchvision.models import EfficientNet as TorchEfficientNet
-from torchvision.models.efficientnet import MBConvConfig, _efficientnet_conf, model_urls
+from torchvision.models.efficientnet import MBConvConfig, model_urls
 
 from mllooper.models import Model, ModelConfig
 
 
 def efficientnet_model(
         arch: str,
-        inverted_residual_setting: List[MBConvConfig],
+        width_mult: float,
+        depth_mult: float,
         pretrained: bool = False,
         progress: bool = True,
         in_channels: int = 3,
         num_classes: int = 1000,
         **kwargs: Any
 ) -> TorchEfficientNet:
+    bneck_conf = partial(MBConvConfig, width_mult=width_mult, depth_mult=depth_mult)
+    inverted_residual_setting = [
+        bneck_conf(1, 3, 1, 32, 16, 1),
+        bneck_conf(6, 3, 2, 16, 24, 2),
+        bneck_conf(6, 5, 2, 24, 40, 2),
+        bneck_conf(6, 3, 2, 40, 80, 3),
+        bneck_conf(6, 5, 1, 80, 112, 3),
+        bneck_conf(6, 5, 2, 112, 192, 4),
+        bneck_conf(6, 3, 1, 192, 320, 1),
+    ]
     model = TorchEfficientNet(inverted_residual_setting, num_classes=num_classes, **kwargs)
 
     if in_channels != 3:
@@ -54,8 +65,8 @@ def efficientnet_model(
     return model
 
 
-b0 = partial(efficientnet_model, "efficientnet_b0", _efficientnet_conf(width_mult=1.0, depth_mult=1.0))
-b1 = partial(efficientnet_model, "efficientnet_b1", _efficientnet_conf(width_mult=1.0, depth_mult=1.1))
+b0 = partial(efficientnet_model, "efficientnet_b0", width_mult=1.0, depth_mult=1.0)
+b1 = partial(efficientnet_model, "efficientnet_b1", width_mult=1.0, depth_mult=1.1)
 
 
 class EfficientNet(Model):
