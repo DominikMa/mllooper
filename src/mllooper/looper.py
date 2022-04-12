da4b6d35a590704f18b74a3beb4e4f94fce4e90f
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2022-04-12 18:13:05 +0200

    Typo

--- src/mllooper/looper.py
+++ src/mllooper/looper.py
@@ -157,7 +157,7 @@ class Looper(Module):
 
             # self.logger.info(f"Doing {self._iterations_per_second:0.2f} iterations per second")
             self.logger.info(f"Iteration {self.inner_state.looper_state.total_iteration} "
-                             f"({self._iterations_per_second:0.2f} it/s")
+                             f"({self._iterations_per_second:0.2f} it/s)")
 
         for _, module in iterate_modules(self.modules, skip_references=True):
             module.log(self.inner_state)
