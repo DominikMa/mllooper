9d95fa49ceb956079ef3b263b86a57d66fce52dd
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2022-12-29 10:50:32 +0100

    Add initialisation and teardown tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,38 @@
+from typing import Dict
+
 import pytest
 
-from mllooper import State
+from mllooper import State, Module, ModuleConfig
 
 
 @pytest.fixture
 def empty_state():
     return State()
+
+
+@pytest.fixture
+def initialise_and_teardown_counter_class():
+    class CounterClass(Module):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            self.count_initialise = 0
+            self.count_teardown = 0
+
+        def step(self, state: State) -> None:
+            pass
+
+        def initialise(self, modules: Dict[str, 'Module']) -> None:
+            self.count_initialise += 1
+
+        def teardown(self, modules: Dict[str, 'Module']) -> None:
+            self.count_teardown += 1
+
+    return CounterClass
+
+
+@pytest.fixture
+def initialise_and_teardown_counter_class_config(initialise_and_teardown_counter_class):
+    class CounterClassConfig(ModuleConfig, loaded_class=initialise_and_teardown_counter_class):
+        pass
+
+    return CounterClassConfig
--- tests/test_looper.py
+++ tests/test_looper.py
@@ -1,7 +1,7 @@
 import pydantic
 import pytest
 
-from mllooper import LooperConfig, NOPConfig
+from mllooper import LooperConfig, NOPConfig, LooperIterationStopConfig, LooperIterationStop, Looper
 
 
 def test_extra_key_module_in_modules():
@@ -44,3 +44,51 @@ def test_missing_reference():
                 'ref_name': 'ref'
             }
         )
+
+def test_initialise_and_teardown_is_called(initialise_and_teardown_counter_class):
+    counter = initialise_and_teardown_counter_class()
+    looper = Looper(
+        modules={
+            'module': counter,
+            'stop': LooperIterationStop(step_iteration_limit=1)
+        }
+    )
+    looper.run()
+
+    assert counter.count_initialise == 1
+    assert counter.count_teardown == 1
+
+
+def test_initialise_and_teardown_for_references_is_called(initialise_and_teardown_counter_class):
+    counter = initialise_and_teardown_counter_class()
+    looper = Looper(
+        modules={
+            'module': counter,
+            'module2': 'module',
+            'another_module': 'module',
+            'stop': LooperIterationStop(step_iteration_limit=1)
+        }
+    )
+    looper.run()
+
+    assert counter.count_initialise == 3
+    assert counter.count_teardown == 3
+
+
+def test_initialise_and_teardown_for_references_is_called_config(initialise_and_teardown_counter_class_config):
+    config = LooperConfig(
+        modules={
+            'module': initialise_and_teardown_counter_class_config(),
+            'module2': 'module',
+        },
+        another_module='module',
+        another_module2='module',
+        stop=LooperIterationStopConfig(step_iteration_limit=1)
+    )
+    looper = config.load()
+    counter = looper.modules['module']
+
+    looper.run()
+
+    assert counter.count_initialise == 4
+    assert counter.count_teardown == 4
