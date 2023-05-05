b57f687bf088d27e590e7c3d30847679d3a2c513
Dominik Mairhöfer <dominik@mairhoefer.xyz>
2023-05-05 15:26:47 +0200

    Update efficientnet model for pytorch 2

--- src/mllooper/models/efficientnet/model.py
+++ src/mllooper/models/efficientnet/model.py
@@ -1,38 +1,30 @@
 from functools import partial
 from typing import Any, Literal
 
+import torchvision
+
 from torch import nn
-from torch.hub import load_state_dict_from_url
-from torchvision.models import EfficientNet as TorchEfficientNet
-from torchvision.models.efficientnet import MBConvConfig, model_urls
+from torchvision.models import EfficientNet as TorchEfficientNet, EfficientNet_B0_Weights, EfficientNet_B1_Weights
 
 from mllooper.models import Model, ModelConfig
 
 
 def efficientnet_model(
         arch: str,
-        width_mult: float,
-        depth_mult: float,
+        weights,
         pretrained: bool = False,
-        progress: bool = True,
+        progress: bool = False,
         in_channels: int = 3,
         num_classes: int = 1000,
         **kwargs: Any
 ) -> TorchEfficientNet:
-    bneck_conf = partial(MBConvConfig, width_mult=width_mult, depth_mult=depth_mult)
-    inverted_residual_setting = [
-        bneck_conf(1, 3, 1, 32, 16, 1),
-        bneck_conf(6, 3, 2, 16, 24, 2),
-        bneck_conf(6, 5, 2, 24, 40, 2),
-        bneck_conf(6, 3, 2, 40, 80, 3),
-        bneck_conf(6, 5, 1, 80, 112, 3),
-        bneck_conf(6, 5, 2, 112, 192, 4),
-        bneck_conf(6, 3, 1, 192, 320, 1),
-    ]
-    model = TorchEfficientNet(inverted_residual_setting, num_classes=num_classes, **kwargs)
+    model_cunstructor = getattr(torchvision.models, arch)
+    model: TorchEfficientNet = model_cunstructor(weights=None, progress=False, num_classes=num_classes, **kwargs)
+
 
     if in_channels != 3:
         original_layer: nn.Conv2d = model.features[0][0]
+
         input_layer = nn.Conv2d(in_channels, original_layer.out_channels, kernel_size=original_layer.kernel_size,
                                 stride=original_layer.stride, padding=original_layer.padding,
                                 dilation=original_layer.dilation, groups=original_layer.groups,
@@ -44,9 +36,7 @@ def efficientnet_model(
         model.features[0][0] = input_layer
 
     if pretrained:
-        if model_urls.get(arch, None) is None:
-            raise ValueError("No checkpoint is available for model type {}".format(arch))
-        state_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
+        state_dict = weights.get_state_dict(progress=progress)
         strict = True
 
         if in_channels != 3:
@@ -65,8 +55,8 @@ def efficientnet_model(
     return model
 
 
-b0 = partial(efficientnet_model, "efficientnet_b0", width_mult=1.0, depth_mult=1.0)
-b1 = partial(efficientnet_model, "efficientnet_b1", width_mult=1.0, depth_mult=1.1)
+b0 = partial(efficientnet_model, "efficientnet_b0", EfficientNet_B0_Weights.IMAGENET1K_V1)
+b1 = partial(efficientnet_model, "efficientnet_b1", EfficientNet_B1_Weights.IMAGENET1K_V1)
 
 
 class EfficientNet(Model):
